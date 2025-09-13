da933b41ea9ca6f8419c441396389f66088cf10d
njabulo moshifa <107797576+moshifa10@users.noreply.github.com>
2025-09-14 00:34:55 +0200

    My second commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # learning-git
+
 I am learning Git
+
 ## git-vscode
+
 I am learning git with Vscode
+
+I will learn it up until I master it
--- main.py
+++ main.py
@@ -2,3 +2,6 @@ git = "I am learning git with vs code"
 monster = "I will be great/comfortable with it"
 
 print(f"{git} and {monster}")
+
+for i in range(10):
+    print(i)
